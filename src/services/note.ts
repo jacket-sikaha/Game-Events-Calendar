076d15581746f6385cb19d72852ff2efd88842e9
sikara <987555458@qq.com>
2024-06-16 14:43:55 +0800

    feat 更新后端url

--- src/services/note.ts
+++ src/services/note.ts
@@ -1,3 +1,4 @@
 // import request from "../utils/request";
 
-export const BACKEND_URL = "https://aaa.sikara.asia";
+// export const BACKEND_URL = "https://aaa.sikara.asia";
+export const BACKEND_URL = "https://gebackend.sikara.workers.dev";
